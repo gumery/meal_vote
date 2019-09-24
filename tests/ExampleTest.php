5e9e10d64f6deb39b99f5fe842ffa5e12f330c12
root <root@localhost.localdomain>
2019-09-24 14:55:02 +0800

    add:添加代码

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,19 +2,19 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2017 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
+namespace tests;
 
-namespace think;
+class ExampleTest extends TestCase
+{
 
-// ThinkPHP 引导文件
-// 加载基础文件
-require __DIR__ . '/base.php';
-
-// 执行应用
-App::initCommon();
-Console::init();
+    public function testBasicExample()
+    {
+        $this->visit('/')->see('ThinkPHP');
+    }
+}
